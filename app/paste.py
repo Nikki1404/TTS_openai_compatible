1f27bb301124237d1341ec4522f1679acbc1310b
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2025-11-24 15:18:42 +0530

    Update paste.py

--- app/paste.py
+++ app/paste.py
@@ -1,32 +1,30 @@
-FROM nvidia/cuda:12.2.2-runtime-ubuntu22.04
+FROM nvidia/cuda:12.1.1-runtime-ubuntu22.04
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+ENV https_proxy="http://163.116.128.80:8080"
+ENV http_proxy="http://163.116.128.80:8080"
 
 WORKDIR /app
 
-# Step 1: Install system dependencies + Python
 RUN apt-get update && apt-get install -y \
     git ffmpeg python3 python3-pip && \
     rm -rf /var/lib/apt/lists/*
 
-# Step 2: Environment variables (AFTER Python exists)
 ENV PYTHONDONTWRITEBYTECODE=1 \
     PYTHONUNBUFFERED=1 \
     KOKORO_LANG=a \
     KOKORO_DEFAULT_VOICE=af_heart \
     KOKORO_PRELOAD_VOICES="af_heart af_bella af_sky"
 
-
 COPY . /app/
 
 RUN --mount=type=cache,target=/root/.cache/pip pip install -r /app/requirements.txt
 
-# Step 4: Install ONNX GPU runtime
 RUN pip install onnxruntime-gpu
-
-
 
 EXPOSE 8080
 
-# Step 6: Run the app
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8080"]
 
 
