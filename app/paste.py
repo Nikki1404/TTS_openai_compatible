471f30f13da44ea87570a9eefca61e8204349fa9
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2025-11-24 16:50:53 +0530

    Update paste.py

--- app/paste.py
+++ app/paste.py
@@ -52,110 +52,6 @@ us-central1-docker.pkg.dev/emr-dgt-autonomous-uctr1-snbx/cx-speech/kokoro:latest
 
 
 https://console.cloud.google.com/artifacts/docker/emr-dgt-autonomous-uctr1-snbx/us-central1/cx-speech?project=emr-dgt-autonomous-uctr1-snbx
-Collecting weasel<0.5.0,>=0.4.2
-  Downloading weasel-0.4.3-py3-none-any.whl (50 kB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 50.8/50.8 KB 186.8 MB/s eta 0:00:00
-Collecting spacy-loggers<2.0.0,>=1.0.0
-  Downloading spacy_loggers-1.0.5-py3-none-any.whl (22 kB)
-Requirement already satisfied: setuptools in /usr/lib/python3/dist-packages (from spacy->misaki[en]>=0.9.4->kokoro->-r /app/requirements.txt (line 8)) (59.6.0)
-Collecting cymem<2.1.0,>=2.0.2
-  Downloading cymem-2.0.13-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.whl (229 kB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 229.7/229.7 KB 295.9 MB/s eta 0:00:00
-Collecting thinc<8.4.0,>=8.3.4
-  Downloading thinc-8.3.10-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.whl (3.9 MB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 3.9/3.9 MB 288.9 MB/s eta 0:00:00
-Collecting wasabi<1.2.0,>=0.9.1
-  Downloading wasabi-1.1.3-py3-none-any.whl (27 kB)
-Collecting catalogue<2.1.0,>=2.0.6
-  Downloading catalogue-2.0.10-py3-none-any.whl (17 kB)
-Collecting preshed<3.1.0,>=3.0.2
-  Downloading preshed-3.0.12-cp310-cp310-manylinux1_x86_64.manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_5_x86_64.whl (780 kB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 780.3/780.3 KB 321.4 MB/s eta 0:00:00
-Collecting srsly<3.0.0,>=2.4.3
-  Downloading srsly-2.5.2-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.whl (1.1 MB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 1.1/1.1 MB 326.6 MB/s eta 0:00:00
-Collecting murmurhash<1.1.0,>=0.28.0
-  Downloading murmurhash-1.0.15-cp310-cp310-manylinux1_x86_64.manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_5_x86_64.whl (122 kB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 122.6/122.6 KB 280.8 MB/s eta 0:00:00
-Collecting curated-transformers<3.0.0,>=2.0.0
-  Downloading curated_transformers-2.0.1-py2.py3-none-any.whl (363 kB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 363.5/363.5 KB 73.7 MB/s eta 0:00:00
-Collecting curated-tokenizers<3.0.0,>=2.0.0
-  Downloading curated_tokenizers-2.0.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl (772 kB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 772.8/772.8 KB 277.4 MB/s eta 0:00:00
-Collecting spacy-curated-transformers
-  Downloading spacy_curated_transformers-2.1.1-py2.py3-none-any.whl (240 kB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 240.3/240.3 KB 290.0 MB/s eta 0:00:00
-Collecting spacy
-  Downloading spacy-4.0.0.dev3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl (6.7 MB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 6.7/6.7 MB 211.2 MB/s eta 0:00:00
-Collecting spacy-curated-transformers
-  Downloading spacy_curated_transformers-2.0.0-py2.py3-none-any.whl (240 kB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 240.3/240.3 KB 299.8 MB/s eta 0:00:00
-  Downloading spacy_curated_transformers-0.3.1-py2.py3-none-any.whl (237 kB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 237.9/237.9 KB 298.0 MB/s eta 0:00:00
-Collecting curated-tokenizers<0.1.0,>=0.0.9
-  Downloading curated_tokenizers-0.0.9-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl (731 kB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 731.6/731.6 KB 316.9 MB/s eta 0:00:00
-Collecting curated-transformers<0.2.0,>=0.1.0
-  Downloading curated_transformers-0.1.1-py2.py3-none-any.whl (25 kB)
-Collecting confection<1.0.0,>=0.0.1
-  Downloading confection-0.1.5-py3-none-any.whl (35 kB)
-Collecting blis<1.4.0,>=1.3.0
-  Downloading blis-1.3.3-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.whl (11.3 MB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 11.3/11.3 MB 275.6 MB/s eta 0:00:00
-Collecting cloudpathlib<1.0.0,>=0.7.0
-  Downloading cloudpathlib-0.23.0-py3-none-any.whl (62 kB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 62.8/62.8 KB 223.7 MB/s eta 0:00:00
-Collecting smart-open<8.0.0,>=5.2.1
-  Downloading smart_open-7.5.0-py3-none-any.whl (63 kB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 63.9/63.9 KB 232.3 MB/s eta 0:00:00
-Collecting csvw>=1.5.6
-  Downloading csvw-3.7.0-py2.py3-none-any.whl (60 kB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 60.7/60.7 KB 234.4 MB/s eta 0:00:00
-Collecting babel
-  Downloading babel-2.17.0-py3-none-any.whl (10.2 MB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 10.2/10.2 MB 255.2 MB/s eta 0:00:00
-Collecting language-tags
-  Downloading language_tags-1.2.0-py3-none-any.whl (213 kB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 213.4/213.4 KB 303.6 MB/s eta 0:00:00
-Collecting rdflib
-  Downloading rdflib-7.4.0-py3-none-any.whl (569 kB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 569.0/569.0 KB 301.4 MB/s eta 0:00:00
-Collecting python-dateutil
-  Downloading python_dateutil-2.9.0.post0-py2.py3-none-any.whl (229 kB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 229.9/229.9 KB 214.3 MB/s eta 0:00:00
-Collecting uritemplate>=3.0.0
-  Downloading uritemplate-4.2.0-py3-none-any.whl (11 kB)
-Collecting jsonschema
-  Downloading jsonschema-4.25.1-py3-none-any.whl (90 kB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 90.0/90.0 KB 253.9 MB/s eta 0:00:00
-Collecting termcolor
-  Downloading termcolor-3.2.0-py3-none-any.whl (7.7 kB)
-Collecting rfc3986<2
-  Downloading rfc3986-1.5.0-py2.py3-none-any.whl (31 kB)
-Collecting isodate
-  Downloading isodate-0.7.2-py3-none-any.whl (22 kB)
-Collecting wrapt
-  Downloading wrapt-2.0.1-cp310-cp310-manylinux1_x86_64.manylinux_2_28_x86_64.manylinux_2_5_x86_64.whl (113 kB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 113.7/113.7 KB 252.2 MB/s eta 0:00:00
-Collecting jsonschema-specifications>=2023.03.6
-  Downloading jsonschema_specifications-2025.9.1-py3-none-any.whl (18 kB)
-Collecting referencing>=0.28.4
-  Downloading referencing-0.37.0-py3-none-any.whl (26 kB)
-Collecting rpds-py>=0.7.1
-  Downloading rpds_py-0.29.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl (392 kB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 392.7/392.7 KB 309.7 MB/s eta 0:00:00
-Collecting six>=1.5
-  Downloading six-1.17.0-py2.py3-none-any.whl (11 kB)
-Collecting pyparsing<4,>=2.1.0
-  Downloading pyparsing-3.2.5-py3-none-any.whl (113 kB)
-     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 113.9/113.9 KB 269.6 MB/s eta 0:00:00
-Building wheels for collected packages: docopt
-  Building wheel for docopt (setup.py): started
-  Building wheel for docopt (setup.py): finished with status 'done'
-  Created wheel for docopt: filename=docopt-0.6.2-py2.py3-none-any.whl size=13723 sha256=bf715b75c9973f8f6b8ca3d0f59cb3eddd4161dff44751dcdbd4f5b8515285c1
-  Stored in directory: /tmp/pip-ephem-wheel-cache-5k9fqv81/wheels/fc/ab/d4/5da2067ac95b36618c629a5f93f809425700506f72c9732fac
 Successfully built docopt
 ERROR: Exception:
 Traceback (most recent call last):
@@ -170,12 +66,67 @@ Traceback (most recent call last):
   File "/usr/lib/python3/dist-packages/pip/_internal/resolution/resolvelib/resolver.py", line 276, in get_topological_weights
     assert len(weights) == expected_node_count
 AssertionError
+DEBUG: https://storage.googleapis.com:443 "GET /150916788856.cloudbuild-logs.googleusercontent.com/log-fcc9825e-92c0-43c6-b520-688ed6d606eb.txt HTTP/1.1" 416 169
+DEBUG: Reading GCS logfile: 416 (no new content; keep polling)
+DEBUG: https://cloudbuild.googleapis.com:443 "GET /v1/projects/emr-dgt-autonomous-uctr1-snbx/locations/global/builds/fcc9825e-92c0-43c6-b520-688ed6d606eb?alt=json HTTP/1.1" 200 None
+DEBUG: https://storage.googleapis.com:443 "GET /150916788856.cloudbuild-logs.googleusercontent.com/log-fcc9825e-92c0-43c6-b520-688ed6d606eb.txt HTTP/1.1" 206 217
+DEBUG: Reading GCS logfile: 206 (read 217 bytes)
 The command '/bin/sh -c pip install --no-cache-dir -r /app/requirements.txt' returned a non-zero code: 2
 ERROR
 ERROR: build step 0 "gcr.io/cloud-builders/gcb-internal" failed: step exited with non-zero status: 2
 
+DEBUG: https://cloudbuild.googleapis.com:443 "GET /v1/projects/emr-dgt-autonomous-uctr1-snbx/locations/global/builds/fcc9825e-92c0-43c6-b520-688ed6d606eb?alt=json HTTP/1.1" 200 None
+DEBUG: https://storage.googleapis.com:443 "GET /150916788856.cloudbuild-logs.googleusercontent.com/log-fcc9825e-92c0-43c6-b520-688ed6d606eb.txt HTTP/1.1" 416 169
+DEBUG: Reading GCS logfile: 416 (no new content; keep polling)
+DEBUG: https://storage.googleapis.com:443 "GET /150916788856.cloudbuild-logs.googleusercontent.com/log-fcc9825e-92c0-43c6-b520-688ed6d606eb.txt HTTP/1.1" 416 169
+DEBUG: Reading GCS logfile: 416 (no new content; keep polling)
+DEBUG: https://cloudbuild.googleapis.com:443 "GET /v1/projects/emr-dgt-autonomous-uctr1-snbx/locations/global/builds/fcc9825e-92c0-43c6-b520-688ed6d606eb?alt=json HTTP/1.1" 200 None
+DEBUG: https://storage.googleapis.com:443 "GET /150916788856.cloudbuild-logs.googleusercontent.com/log-fcc9825e-92c0-43c6-b520-688ed6d606eb.txt HTTP/1.1" 416 169
+DEBUG: Reading GCS logfile: 416 (no new content; keep polling)
+DEBUG: https://storage.googleapis.com:443 "GET /150916788856.cloudbuild-logs.googleusercontent.com/log-fcc9825e-92c0-43c6-b520-688ed6d606eb.txt HTTP/1.1" 416 169
+DEBUG: Reading GCS logfile: 416 (no new content; keep polling)
+DEBUG: https://cloudbuild.googleapis.com:443 "GET /v1/projects/emr-dgt-autonomous-uctr1-snbx/locations/global/builds/fcc9825e-92c0-43c6-b520-688ed6d606eb?alt=json HTTP/1.1" 200 None
+DEBUG: https://storage.googleapis.com:443 "GET /150916788856.cloudbuild-logs.googleusercontent.com/log-fcc9825e-92c0-43c6-b520-688ed6d606eb.txt HTTP/1.1" 416 169
+DEBUG: Reading GCS logfile: 416 (no new content; keep polling)
+DEBUG: https://cloudbuild.googleapis.com:443 "GET /v1/projects/emr-dgt-autonomous-uctr1-snbx/locations/global/builds/fcc9825e-92c0-43c6-b520-688ed6d606eb?alt=json HTTP/1.1" 200 None
+DEBUG: https://storage.googleapis.com:443 "GET /150916788856.cloudbuild-logs.googleusercontent.com/log-fcc9825e-92c0-43c6-b520-688ed6d606eb.txt HTTP/1.1" 416 169
+DEBUG: Reading GCS logfile: 416 (no new content; keep polling)
+DEBUG: https://cloudbuild.googleapis.com:443 "GET /v1/projects/emr-dgt-autonomous-uctr1-snbx/locations/global/builds/fcc9825e-92c0-43c6-b520-688ed6d606eb?alt=json HTTP/1.1" 200 None
+DEBUG: https://storage.googleapis.com:443 "GET /150916788856.cloudbuild-logs.googleusercontent.com/log-fcc9825e-92c0-43c6-b520-688ed6d606eb.txt HTTP/1.1" 416 169
+DEBUG: Reading GCS logfile: 416 (no new content; keep polling)
+DEBUG: https://storage.googleapis.com:443 "GET /150916788856.cloudbuild-logs.googleusercontent.com/log-fcc9825e-92c0-43c6-b520-688ed6d606eb.txt HTTP/1.1" 416 169
+DEBUG: Reading GCS logfile: 416 (no new content; keep polling)
+DEBUG: https://cloudbuild.googleapis.com:443 "GET /v1/projects/emr-dgt-autonomous-uctr1-snbx/locations/global/builds/fcc9825e-92c0-43c6-b520-688ed6d606eb?alt=json HTTP/1.1" 200 None
+DEBUG: https://storage.googleapis.com:443 "GET /150916788856.cloudbuild-logs.googleusercontent.com/log-fcc9825e-92c0-43c6-b520-688ed6d606eb.txt HTTP/1.1" 416 169
+DEBUG: Reading GCS logfile: 416 (no new content; keep polling)
+DEBUG: https://storage.googleapis.com:443 "GET /150916788856.cloudbuild-logs.googleusercontent.com/log-fcc9825e-92c0-43c6-b520-688ed6d606eb.txt HTTP/1.1" 416 169
+DEBUG: Reading GCS logfile: 416 (no new content; keep polling)
+DEBUG: https://cloudbuild.googleapis.com:443 "GET /v1/projects/emr-dgt-autonomous-uctr1-snbx/locations/global/builds/fcc9825e-92c0-43c6-b520-688ed6d606eb?alt=json HTTP/1.1" 200 None
+DEBUG: https://storage.googleapis.com:443 "GET /150916788856.cloudbuild-logs.googleusercontent.com/log-fcc9825e-92c0-43c6-b520-688ed6d606eb.txt HTTP/1.1" 416 169
+DEBUG: Reading GCS logfile: 416 (no new content; keep polling)
+DEBUG: https://storage.googleapis.com:443 "GET /150916788856.cloudbuild-logs.googleusercontent.com/log-fcc9825e-92c0-43c6-b520-688ed6d606eb.txt HTTP/1.1" 416 169
+DEBUG: Reading GCS logfile: 416 (no new content; keep polling)
 -------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 BUILD FAILURE: Build step failure: build step 0 "gcr.io/cloud-builders/gcb-internal" failed: step exited with non-zero status: 2
-ERROR: (gcloud.builds.submit) build 9dce7d38-533e-4a5b-a1f4-6fe2d9835494 completed with status "FAILURE"
-re_nikitav@cloudshell:~/fastapi_impl_gpu (emr-dgt-autonomous-uctr1-snbx)$ 
+DEBUG: (gcloud.builds.submit) build fcc9825e-92c0-43c6-b520-688ed6d606eb completed with status "FAILURE"
+Traceback (most recent call last):
+  File "/usr/bin/../lib/google-cloud-sdk/lib/googlecloudsdk/calliope/cli.py", line 944, in Execute
+    resources = calliope_command.Run(cli=self, args=args)
+  File "/usr/bin/../lib/google-cloud-sdk/lib/googlecloudsdk/calliope/backend.py", line 955, in Run
+    resources = command_instance.Run(args)
+  File "/usr/bin/../lib/google-cloud-sdk/lib/surface/builds/submit.py", line 272, in Run
+    build, _ = submit_util.Build(
+               ~~~~~~~~~~~~~~~~~^
+        messages,
+        ^^^^^^^^^
+    ...<4 lines>...
+        suppress_logs=args.suppress_logs,
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        polling_interval=args.polling_interval)
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/usr/bin/../lib/google-cloud-sdk/lib/googlecloudsdk/command_lib/builds/submit_util.py", line 940, in Build
+    raise FailedBuildException(build)
+googlecloudsdk.command_lib.builds.submit_util.FailedBuildException: build fcc9825e-92c0-43c6-b520-688ed6d606eb completed with status "FAILURE"
+ERROR: (gcloud.builds.submit) build fcc9825e-92c0-43c6-b520-688ed6d606eb completed with status "FAILURE"
+re_nikitav@cloudshell:~/fastapi_impl_gpu (emr-dgt-autonomous-uctr1-snbx
