ebec69e1b8847ad9cceb81d681782439ba0b16b1
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2025-12-09 04:36:59 +0530

    Update paste.py

--- app/paste.py
+++ app/paste.py
@@ -1,20 +1,5 @@
-(asr_benchmark_env) root@EC03-E01-AIASR1:/home/CORP/re_nikitav/asr_backend# curl -vk http://cx-apps.exlservice.com/apps/docs
-*   Trying 45.60.48.19:80...
-* Connected to cx-apps.exlservice.com (45.60.48.19) port 80 (#0)
-> GET /apps/docs HTTP/1.1
-> Host: cx-apps.exlservice.com
-> User-Agent: curl/7.81.0
-> Accept: */*
->
-* Mark bundle as not supporting multiuse
-< HTTP/1.1 403 Forbidden
-< Content-Type: text/html
-< Cache-Control: no-cache, no-store
-< Connection: close
-< Content-Length: 753
-< X-Iinfo: 10-80085843-0 0NNN RT(1765205070741 0) q(0 -1 -1 0) r(0 -1) B16 U18
-< Set-Cookie: visid_incap_3254077=gAUOPapoQ7GkkqY6RpzbUE7kNmkAAAAAQUIPAAAAAACs2euRYMefT4vE91Datl5A; expires=Tue, 08 Dec 2026 07:01:10 GMT; HttpOnly; path=/; Domain=.exlservice.com
-< Set-Cookie: incap_ses_1352_3254077=9OPAIjOqY0MxIgUPTUXDEk7kNmkAAAAAW6eT9Q+PtFqA4Qhs8G3QjA==; path=/; Domain=.exlservice.com
-<
-* Closing connection 0
-<html style="height:100%"><head><META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW"><meta name="format-detection" content="telephone=no"><meta name="viewport" content="initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"></head><body style="margin:0px;height:100%"><iframe id="main-iframe" src="/_Incapsula_Resource?CWUDNSAI=1&xinfo=10-80085843-0%200NNN%20RT%281765205070741%200%29%20q%280%20-1%20-1%200%29%20r%280%20-1%29%20B16%20U18&incident_id=1352000510408073777-473054862075497674&edet=16&cinfo=ffffffff&rpinfo=0&cip=54.175.30.239&mth=GET" frameborder=0 width="100%" height="100%" marginheight="0px" marginwidth="0px">Request unsuccessful. Incapsula incident ID: 1352000510408073777-473054862075497674</iframe></body></html>
+(asr_benchmark_env) root@EC03-E01-AIASR1:/home/CORP/re_nikitav/asr_backend# curl http://localhost:3019/apps/benchmarking-test/openai.json | head
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+100    22  100    22    0     0   7198      0 --:--:-- --:--:-- --:--:-- 11000
+{"detail":"Not Found"}(asr_benchmark_env) root@EC03-E01-AIASR1:/home/CORP/re_nikitav/asr_backend#
