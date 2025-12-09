802a2e905aa0b417ef3cf92b3986f4d3f605bb38
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2025-12-09 18:15:35 +0530

    Update gemini.py

--- app/gemini.py
+++ app/gemini.py
@@ -3,3 +3,14 @@ curl -X 'POST' \
   -H 'accept: application/json' \
   -H 'Content-Type: multipart/form-data' \
   -F 'file=@asr_benchmarking_Q2_2025 (1).xlsx;type=application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+
+	
+Error: Internal Server Error
+
+Response body
+Download
+{
+  "detail": "An error occurred while processing the file: 400: Invalid data in row 2: could not convert string to float: '06:44:00'"
+}
+
+getting this 
